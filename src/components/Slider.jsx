15d1730eef11c6fb63e136db364e47b7ed7ec569
Chonthichar <chonthichar.soythong@gmail.com>
2022-05-10 15:27:51 +0700

    styling blogs

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,6 +15,7 @@ import {FaInstagram} from "react-icons/fa";
 import {FaSubscript} from "react-icons/fa";
 import {FaLinkedin} from "react-icons/fa";
 import Ploy from "../asset/blog-ploy.jpg"
+
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 //spinner componnet
@@ -61,37 +62,61 @@ function Slider() {
 
 
             <div className='columnGrid'>
-            <Swiper slidesPerView={1}
-                    pagination={{clickable: true}}
-            >
-                {listings.map(({data, id}) => (
-                    <SwiperSlide key={id} onClick={() => navigate(`/category/${data.type}/${id}`)}>
-                        <div style={{background: `url(${data.imageUrls[0]})
-                        center no-repeat`,
-                        backgroundSize: 'cover',
-                        height: '500px'}}
-                        className='swipperSlider'>
-                            <p className='swiperSlideText'>{data.name}</p>
-                            <p className='swiperSlidePrice'>${data.discountedPrice ?? data.regularPrice}
-                                {data.type === 'rent' && '/month'}
-                            </p>
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <div className='grid'>
-                <img src={Ploy} alt="{Profile" className='profile-photo'/>
-                <h2>Warunee S.</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, cursus velit commodo</p>
-                <h3>Subscribe and Follow</h3>
-                <div className='gridSubscripe'>
-                    <a href="https://www.facebook.com/Ploywaruneest" style={{textDecoration: 'none'}}><button type='button'><FaFacebook className='fa'/> Follow me on</button></a>
-                    <a href="https://www.instagram.com/warunee2465/?hl=en"  style={{textDecoration: 'none'}}><button><FaInstagram className='fa'/>Follow mw on</button></a>
-                    <a href="https://www.linkedin.com/"  style={{textDecoration: 'none'}}><button><FaLinkedin className='fa'/>Follow mw on</button></a>
-                    <a href="https://www.youtube.com/shorts/Q5SoHn-3IN0"  style={{textDecoration: 'none'}}><button><FaSubscript className='fa'/>Subscribe</button></a>
+                <Swiper slidesPerView={1}
+                        pagination={{clickable: true}}
+                        breakpoints={{
+                            // when window width is >= 640px
+                            500: {
+                                width: 500,
+                                slidesPerView: 1,
+                            },
+                            // when window width is >= 768px
+                            // 768: {
+                            //     width: 768,
+                            //     slidesPerView: 1,
+                            // },
+                        }}
+                >
 
+                    {listings.map(({data, id}) => (
+                        <SwiperSlide key={id} onClick={() => navigate(`/category/${data.type}/${id}`)}>
+                            <div style={{
+                                background: `url(${data.imageUrls[0]})
+                        center no-repeat`,
+                                backgroundSize: 'cover',
+                                height: '400px'
+                            }}
+                                 className='swipperSlider'>
+                                <p className='swiperSlideText'>{data.name}</p>
+                                <p className='swiperSlidePrice'>${data.discountedPrice ?? data.regularPrice}
+                                    {data.type === 'rent' && '/month'}
+                                </p>
+                            </div>
+
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className='grid'>
+                    <img src={Ploy} alt="{Profile" className='profile-photo'/>
+                    <h2>Warunee S.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, cursus velit commodo</p>
+                    <h3>Subscribe and Follow</h3>
+                    <div className='gridSubscripe'>
+                        <a href="https://www.facebook.com/Ploywaruneest" style={{textDecoration: 'none'}}>
+                            <button type='button'><FaFacebook className='fa'/> Follow me on</button>
+                        </a>
+                        <a href="https://www.instagram.com/warunee2465/?hl=en" style={{textDecoration: 'none'}}>
+                            <button><FaInstagram className='fa'/>Follow mw on</button>
+                        </a>
+                        <a href="https://www.linkedin.com/" style={{textDecoration: 'none'}}>
+                            <button><FaLinkedin className='fa'/>Follow mw on</button>
+                        </a>
+                        <a href="https://www.youtube.com/shorts/Q5SoHn-3IN0" style={{textDecoration: 'none'}}>
+                            <button><FaSubscript className='fa'/>Subscribe</button>
+                        </a>
+
+                    </div>
                 </div>
-            </div>
             </div>
         </>
     )
