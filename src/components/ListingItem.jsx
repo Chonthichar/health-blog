5c1a6b58dc32b7150956fc43a646758aee7ae405
Chonthichar <chonthichar.soythong@gmail.com>
2022-05-26 22:21:31 +0700

    styling

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,7 +5,7 @@ import {FaEdit} from "react-icons/fa";
 
 function ListingItem({listing, id, onEdit, onDelete}) {
     return (
-        <div className='container new'>
+        <div className=''>
             {/*<li className=''>*/}
                 <Link to={`/category/${listing.type}/${id}`}
                       className='categoryListingLink' style={{textDecoration: 'none'}}>
